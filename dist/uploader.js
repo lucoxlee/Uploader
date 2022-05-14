a68750cf38890150c670459d6238751b7cd42a14
yule <yuleaugustine@gmail.com>
2022-05-14 18:20:54 +0800

    [release] 0.6.19

--- dist/uploader.js
+++ dist/uploader.js
@@ -1,6 +1,6 @@
 /*!
  * Uploader - Uploader library implements html5 file upload and provides multiple simultaneous, stable, fault tolerant and resumable uploads
- * @version v0.6.18
+ * @version v0.6.19
  * @author dolymood <dolymood@gmail.com>
  * @link https://github.com/simple-uploader/Uploader
  * @license MIT
@@ -389,7 +389,7 @@ var event = _dereq_('./event')
 var File = _dereq_('./file')
 var Chunk = _dereq_('./chunk')
 
-var version = '0.6.18'
+var version = '0.6.19'
 
 var isServer = typeof window === 'undefined'
 
